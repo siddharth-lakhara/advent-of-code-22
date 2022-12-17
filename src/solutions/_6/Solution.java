a3aaf364bdbfe2e1e37c3f97fd764ed1d7e7d3aa
lakhara <siddharth.lakhara@gmail.com>
2022-12-17 10:28:37 +0530

    day 6 part 2

--- src/solutions/_6/Solution.java
+++ src/solutions/_6/Solution.java
@@ -87,32 +87,70 @@ public class Solution {
     }
 
     private int solve2() throws IOException {
-        final String fileName = "/part2.input";
+        final String fileName = "/part1.input";
         BufferedReader br = new BufferedReader(
                 new FileReader("resources/" + this.packageName + fileName));
         Scanner inStream = new Scanner(br);
 
-        while (inStream.hasNextLine()) {
-            String input = inStream.nextLine();
-            if (input.length() > 0) {
+        StringBuffer sbf = new StringBuffer();
+        sbf.append(inStream.nextLine());
+        sbf.trimToSize();
 
+        int markerStart = 0;
+        int uniqueCounter = 0;
+        HashMap<Character, Integer> freqCounter = new HashMap<>();
+
+        final int COUNTER_LIMIT = 14;
+
+        for (; markerStart < sbf.length(); markerStart += 1) {
+//            Found ?? checking here to compensate 0 based idx
+            if (uniqueCounter == COUNTER_LIMIT) {
+                break;
             }
+
+            //            remove item came off window
+            if (markerStart - COUNTER_LIMIT >= 0) {
+                char oldChar = sbf.charAt(markerStart - COUNTER_LIMIT);
+                int oldFreq = freqCounter.get(oldChar);
+                if (oldFreq == 2) {
+                    uniqueCounter++;
+                    freqCounter.put(oldChar, oldFreq - 1);
+                } else if (oldFreq == 1) {
+                    freqCounter.remove(oldChar);
+                    uniqueCounter--;
+                } else {
+                    freqCounter.put(oldChar, oldFreq - 1);
+                }
+            }
+
+//            add item came into window
+            char ch = sbf.charAt(markerStart);
+            if (freqCounter.containsKey(ch)) {
+                if (freqCounter.get(ch) == 1) {
+                    uniqueCounter--;
+                }
+                freqCounter.put(ch, freqCounter.get(ch) + 1);
+            } else {
+                freqCounter.put(ch, 1);
+                uniqueCounter++;
+            }
+
 
         }
 
         inStream.close();
-        return 0;
+        return markerStart;
     }
 
     public void solve() throws IOException {
-        int answer0 = solveExample();
-        System.out.println("Solution0: " + answer0);
+//        int answer0 = solveExample();
+//        System.out.println("Solution0: " + answer0);
 
 //        int answer1 = solve1();
 //        System.out.println("Solution1: " + answer1);
 //
-//        int answer2 = solve2();
-//        System.out.println("Solution2: " + answer2);
+        int answer2 = solve2();
+        System.out.println("Solution2: " + answer2);
     }
 }
 
